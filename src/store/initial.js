971a627a70acf75703e27a3251714901dfc159c4
Walmik Deshpande <walmik.deshpande@gmail.com>
2018-09-22 21:16:48 -0700

    Use simplified sampler

--- src/store/initial.js
+++ src/store/initial.js
@@ -1,37 +1,16 @@
 export default function getInitialState() {
   return {
-    channels: [{
-      name: 'Kk',
-      clips: [
-        { pattern: 'x' },
-        { pattern: 'x' }
-      ],
-      sample: '/sounds/hits/kick.wav'
-    },
-    {
-      name: 'B',
-      clips: [
-        { pattern: '[-x]' },
-        { pattern: '[xx]' }
-      ],
-      sample: '/sounds/hits/bass.wav'
-    },
+    channels: [
     {
       name: 'P',
       clips: [
-        { pattern: 'x', notes: 'c4' },
-        { pattern: '[xx]', notes: 'd4' },
+        { pattern: 'x', notes: 'G3' },
+        { pattern: 'x' }
       ],
-      samples: samplers.piano
-    },
-    {
-      name: 'Hats',
-      clips: [
-        { pattern: '[-x]' },
-        { pattern: '[xx]' }
-      ],
-      sample: '/sounds/hits/ch.wav'
-    }].map((ch, idx) => {
+      samples: samplers.piano,
+      volume: -24
+    }
+    ].map((ch, idx) => {
       ch.id = idx;
       ch.currentlyPlayingClipIdx = -1;
       return ch;
