e9ef54be99b00eac9eae3eba4ced321e70cc4c5d
Walmik <walmik.deshpande@gmail.com>
2018-12-09 20:53:38 -0800

    Update example to reproduce note length issue

--- src/example.js
+++ src/example.js
@@ -1,14 +1,13 @@
 // Inspiration https://codepen.io/jakealbaugh/full/qNrZyw
 
-import { clip, scale, chord, progression } from 'scribbletune';
+import { clip, arp, progression } from 'scribbletune';
 
 // get the notes in the scale
-const theChords = progression.getChords('C4 major', 'I I ii ii III IV III V');
+const arpedChords = arp(progression.getChords('C4 harmonic major', 'i i ii ii III IV III V'));
 
 clip({
-  notes: progression.arpegiate(theChords),
-  subdiv: '16n',
-  pattern: 'x-xx',
+  notes: arpedChords,
+  pattern: 'x___'.repeat((arpedChords.length)),
   synth: 'Synth'
 }).start();
 
