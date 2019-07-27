2b753f0bc406100d39ad9de0ecb0be507ee9a521
Walmik <walmik.deshpande@gmail.com>
2019-07-27 14:11:59 -0700

    Init

--- src/modules/rpg8rs/actions.js
+++
@@ -1,4 +0,0 @@
-export const play = dispatch => (dispatch({type: 'PLAY' }));
-export const stop = dispatch => (dispatch({type: 'STOP' }));
-export const saveMidi = dispatch => (dispatch({type: 'SAVE_MIDI' }));
-export const updateClip = (dispatch, data) => (dispatch({type: 'UPDATE_CLIP', data }));
