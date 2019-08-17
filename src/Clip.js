20dd1a1f9cd86c6ce78fe31ab127910ba1370d57
Walmik <walmik.deshpande@gmail.com>
2019-08-17 12:39:56 -0700

    Bring in rudimentary clip editor UI

--- src/Clip.js
+++ src/Clip.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Mutation } from 'react-apollo';
-import { Button } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  ButtonGroup,
+  Button,
+  Modal,
+} from 'react-bootstrap';
 import { STOP_CLIP, PLAY_CLIP } from './gql';
 
 function Clip(props) {
+  const [showModal, setShowModal] = useState(false);
+  // const [isPlaying, setIsPlaying] = useState(false);
+  const [pattern, setPattern] = useState(props.pattern || '');
+  const [notes, setNotes] = useState(props.notes || '');
+  const [randomNotes, setRandomNotes] = useState(props.randomNotes || '');
+  const [subdiv, setSubdiv] = useState(props.subdiv || '4n');
+  const [dur, setDur] = useState(props.dur || '4n');
+
   const getClipButton = () => {
-    if (props.pattern === '') {
-      return <Button variant="outline-secondary">&#x25CB;</Button>;
+    if (!pattern) {
+      return (
+        <Button variant="outline-secondary" disabled={true}>
+          &#x25CB;
+        </Button>
+      );
     }
 
     if (props.activeClipIdx === props.idx) {
@@ -42,7 +61,90 @@ function Clip(props) {
     }
   };
 
-  return <div className="clip">{getClipButton()}</div>;
+  return (
+    <div className="clip">
+      <ButtonGroup>
+        {getClipButton()}
+        <Button
+          variant={pattern ? 'secondary' : 'outline-secondary'}
+          onClick={() => setShowModal(true)}
+          dangerouslySetInnerHTML={{
+            __html: '⚙',
+          }}
+        />
+      </ButtonGroup>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col md={4}>Pattern</Col>
+              <Col md={8}>
+                <input
+                  onChange={e => setPattern(e.target.value)}
+                  value={pattern}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={4}>Notes</Col>
+              <Col md={8}>
+                <textarea
+                  onChange={e => setNotes(e.target.value)}
+                  value={notes}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={4}>Random notes</Col>
+              <Col md={8}>
+                <input
+                  onChange={e => setRandomNotes(e.target.value)}
+                  value={randomNotes}
+                />
+              </Col>
+            </Row>
+          </Container>
+
+          <Container>
+            <Row>
+              <Col>
+                subdiv:
+                <select
+                  defaultValue={subdiv}
+                  onChange={e => setSubdiv(e.target.value)}
+                >
+                  <option>1n</option>
+                  <option>2n</option>
+                  <option>4n</option>
+                  <option>8n</option>
+                  <option>16n</option>
+                  <option>32n</option>
+                </select>
+              </Col>
+              <Col>
+                dur:
+                <select
+                  defaultValue={dur}
+                  onChange={e => setDur(e.target.value)}
+                >
+                  <option>1n</option>
+                  <option>2n</option>
+                  <option>4n</option>
+                  <option>8n</option>
+                  <option>16n</option>
+                  <option>32n</option>
+                </select>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
 export default Clip;
